a0c23af92dbc327843c868255499d24587958690
Carlos Soto <carlos.soto@advania.se>
2026-01-08 12:40:16 +0100

    labb 3 klar

--- myfunctions.ps1
+++ myfunctions.ps1
@@ -9,6 +9,23 @@ enum ColorEnum {
     blue
 }
 
+class Participant {
+    [string] $Name
+    [int] $age
+    [ColorEnum] $color
+    [int] $id
+
+    Participant([string]$name,[int]$age,[ColorEnum]$color,[int]$id){
+        $this.name = $name
+        $this.age = $age
+        $this.color = $color
+        $this.id = $id
+    }
+
+     [string] ToString() {
+        Return '{0},{1},{2},{3}' -f $This.Name, $This.Age, $This.Color, $This.Id
+    }
+}
 function Get-CourseUser {
     [CmdletBinding()]
     param (
@@ -36,9 +53,11 @@ function add-courseuser {
     if(!$UserId){
         $UserId = Get-Random -Minimum 10 -Maximum 100000
     }
-    write-host $name $age $color $UserId
-
-    $MyCsvUser = "$Name,$Age,{0},{1}" -f $Color, $UserId
+    $MyNewUser = [Participant]::new($Name, $Age, $Color, $UserId)
+    $MyNewUser
+    $MyCsvUser = $MyNewUser.ToString()
+    $MyCsvUser
+    #$MyCsvUser = "$Name,$Age,{0},{1}" -f $Color, $UserId
 
     $NewCSv = Get-Content $databasefile -Raw
     $NewCSv += $MyCsvUser
